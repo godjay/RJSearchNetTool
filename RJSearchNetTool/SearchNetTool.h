a39fa835237181bcbc144abb58aa213a1786c5c5
lirenjie <lirenjie@appledeMac-mini.local>
2016-04-01 14:45:36 +0800

    well

---
+++ RJSearchNetTool/SearchNetTool.h
@@ -0,0 +1,12 @@
+//
+//  SearchNetTool.h
+
+
+#import <Foundation/Foundation.h>
+
+@interface SearchNetTool : NSObject
++ (void)searchnet;
++ (void)searchnetWithView:(UIView *)view;
+
++ (void)stopSearch;
+@end
---
+++ RJSearchNetTool/SearchNetTool.m
@@ -0,0 +1,77 @@
+//
+//  SearchNetTool.m
+
+#import "SearchNetTool.h"
+#import <AFNetworking.h>
+@implementation SearchNetTool
++ (void)searchnet{
+    //检测网络
+    AFNetworkReachabilityManager *mgr = [AFNetworkReachabilityManager sharedManager];
+    [mgr setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        // 当网络状态发生改变的时候调用这个block
+        switch (status) {
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                NSLog(@"WIFI");
+                break;
+                
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                NSLog(@"自带网络");
+                break;
+                
+            case AFNetworkReachabilityStatusNotReachable:
+                NSLog(@"没有网络");
+                [ProgressHUD dismiss];
+                [ProgressHUD showError:@"请检查网络设置！"];
+                
+                break;
+                
+            case AFNetworkReachabilityStatusUnknown:
+                NSLog(@"未知网络");
+                break;
+            default:
+                break;
+        }
+    }];
+    // 开始监控
+    [mgr startMonitoring];
+}
+
++ (void)searchnetWithView:(UIView *)view{
+    //检测网络
+    AFNetworkReachabilityManager *mgr = [AFNetworkReachabilityManager sharedManager];
+    [mgr setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        // 当网络状态发生改变的时候调用这个block
+        switch (status) {
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                NSLog(@"WIFI");
+                break;
+                
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                NSLog(@"自带网络");
+                break;
+                
+            case AFNetworkReachabilityStatusNotReachable:
+                NSLog(@"没有网络");
+                [ProgressHUD dismiss];
+                [ProgressHUD showError:@"请检查网络设置！"];
+                view.userInteractionEnabled = YES;
+                break;
+                
+            case AFNetworkReachabilityStatusUnknown:
+                NSLog(@"未知网络");
+                break;
+            default:
+                break;
+        }
+    }];
+    // 开始监控
+    [mgr startMonitoring];
+    
+}
+
++ (void)stopSearch{
+    [[AFNetworkReachabilityManager sharedManager] stopMonitoring];
+}
+
+
+@end
